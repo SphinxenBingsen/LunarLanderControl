3be9244b4484280bd8f3d0661bde7b1a5cd21832
chris <cralleo2000@gmai.com>
2025-09-03 15:05:36 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿int Control(int altitude)
+{
+    int thruster = 0;
+
+    if (altitude > 100)
+    {
+        thruster = 0;
+    }
+    else if (altitude > 0)
+    {
+        thruster = 1;
+    }
+    else
+    {
+        thruster = 0;
+    }
+
+    return thruster;
+}
+
+void Test(int altitude)
+{
+    int thruster = Control(altitude);
+    bool behaviorCorrect = (altitude > 100 && thruster == 0) ||
+                           (altitude is <= 100 and > 0 && thruster == 1) ||
+                           (altitude <= 0 && thruster == 0);
+    var behaviorCorrectIcon = behaviorCorrect ? "v" : "✗"; /* I changed emojis to "v / x" since it could not
+    recognise it*/
+    
+    Console.WriteLine($"For altitude {altitude}, your thruster is {thruster} |{behaviorCorrectIcon}|");
+}
+
+Test(150);
+Test(100);
+Test(50);
+Test(0);
+Test(-1);
